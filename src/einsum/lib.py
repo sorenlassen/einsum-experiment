e627c43c66f2462e7697fd1c1cbb6456aacadac8
Soren Lassen <sorenlassen@gmail.com>
2022-04-20 21:53:47 -0700

    documented how output ellipsis checks follow torch/onnx

--- src/einsum/lib.py
+++ src/einsum/lib.py
@@ -122,6 +122,13 @@ def einsum_output(
     else:
         lst = [ s.replace(" ", "") for s in subscripts.split("...") ]
         assert 1 <= len(lst) <= 2, f"multiple ellipses in '{subscripts}'"
+
+        # following torch and onnx, we don't require that the output has
+        # an ellipsis even if any appears in the inputs, whereas to follow
+        # numpy we'd require:
+        #
+        #   if einsum_ellipsis_idxs(idxs_map) != []: assert len(lst) == 2
+
         duplicate = einsum_find_duplicate("".join(lst))
         assert duplicate is None, f"duplicate index {duplicate} in '{subscripts}'"
 
